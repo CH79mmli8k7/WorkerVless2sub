2063606fe39627e22f4666a31b536679580fad2a
edachan <echan8608@gmail.com>
2024-08-05 20:06:11 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,9 +5,10 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
-	'icook.tw:2053#官方优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
-	'sp.rweek.top',
+	'hk.edachantv.sbs#优选香港',
+	'jp.edachantv.sbs#优选日本',
+	'us.edachantv.sbs#优选美国',
+	'sg.edachantv.sbs#优选新加坡',
 ];
 
 // 设置优选地址api接口
